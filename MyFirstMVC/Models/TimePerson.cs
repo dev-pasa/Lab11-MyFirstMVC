b5923926c4d99f48e1f9bedb86b5d0598be72566
dev-pasa <dev5@uw.edu>
2019-01-24 00:27:31 -0800

    read add

---
+++ MyFirstMVC/Models/TimePerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Models
+{
+    public class TimePerson
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Birth_Year { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+
+
+        public static string[] ReadFile(string filePath)
+        {
+
+            string setRoot = Environment.CurrentDirectory;
+            string finalPath = Path.GetFullPath(Path.Combine(setRoot, filePath));
+            string[] rawData = File.ReadAllLines(filePath);
+
+            return rawData;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # My_First_MVC
+# Summary
+This is my first ASP.NET Core MVC web application. Create a web app that will allow a user to put in a span of 2 different years, which will in turn generate a list of all the winners fot Time magazine.
+
+
+# Visuals
+
+# Directions
+Select two different years and hit submit.
+
+# Relevant details
+Still incomplete
